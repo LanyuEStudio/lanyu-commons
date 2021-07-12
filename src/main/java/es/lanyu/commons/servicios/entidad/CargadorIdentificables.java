1760cebb4f61b6a291a2339600edf2227830615a
Awes0meM4n <lonchezas@hotmail.com>
2021-07-12 21:24:57 +0100

    Mejoras en CargadorIdentificables
    * Ahora sirve para mas identificadores `Comparable`s, no sólo `String`
    * Añadido método `getDeserializadorDefecto()` para mejorar extensión

--- src/main/java/es/lanyu/commons/servicios/entidad/CargadorIdentificables.java
+++ src/main/java/es/lanyu/commons/servicios/entidad/CargadorIdentificables.java
@@ -1,46 +1,96 @@
 package es.lanyu.commons.servicios.entidad;
 
+import es.lanyu.commons.identificable.AbstractNombrable;
 import es.lanyu.commons.identificable.GestorIdentificables;
 import es.lanyu.commons.identificable.Identificable;
 import es.lanyu.commons.identificable.Nombrable;
+import es.lanyu.commons.io.Deserializador;
 
-/**Interfaz para cargar gestores tipo {@link GestorIdentificables}.
+/**
+ * Interfaz para cargar gestores tipo {@link GestorIdentificables}.
+ * 
  * @author <a href="https://github.com/Awes0meM4n">Awes0meM4n</a>
  * @version 1.0
  * @since 1.0
  */
 public interface CargadorIdentificables {
 
-	/**Facilita la carga de {@link Identificable} proporcionando un serializador por defecto
-	 * para los subtipos de {@link AbstractNombrable} ({@code id} y {@code nombre}).
-	 * @param <T> Tipo de la {@code claseMapa}
-	 * @param <S> Tipo de la especializacion. Puede ser el igual a {@code T}
-	 * @param rutaArchivo ruta al archivo con los datos
-	 * @param claseMapa Clase que sirve para mapear el recurso (normalmente la mas generica)
-	 * @param claseEspecializacion Clase subtipo de claseMapa para gestionar recursos mas especializados
-	 * @param gestor Gestor que administrara los recursos a cargar
-	 */
-	<T extends Identificable<String> & Nombrable, S extends T> void cargarNombrables (
-			String rutaArchivo,
-			Class<T> claseMapa,
-			Class<S> claseEspecializacion,
-			GestorIdentificables gestor);
-	
-	/**Sobrecarga de
-	 * {@link CargadorIdentificables#cargarNombrables(String, Class, Class, GestorIdentificables)}
-	 * utilizando un {@link ServicioEntidad}.
-	 * @param <T> Tipo de la {@code claseMapa}
-	 * @param <S> Tipo de la especializacion. Puede ser el igual a {@code T}
-	 * @param rutaArchivo ruta al archivo con los datos
-	 * @param claseMapa Clase que sirve para mapear el recurso (normalmente la mas generica)
-	 * @param claseEspecializacion Clase subtipo de claseMapa para gestionar recursos mas especializados
-	 * @param servicio {@code ServicioEntidad} que administrara los recursos a cargar
-	 */
-	default <T extends Identificable<String> & Nombrable, S extends T> void cargarNombrables (
-			String rutaArchivo,
-			Class<T> claseMapa,
-			Class<S> claseEspecializacion,
-			ServicioEntidad servicio){
-		cargarNombrables(rutaArchivo, claseMapa, claseEspecializacion, servicio.getGestorNombrables());
-	}
+    /**
+     * Devuelve un deserializador por defecto.
+     * 
+     * @return deserializador por defecto
+     */
+    Deserializador getDeserializadorDefecto();
+
+    /**
+     * Facilita la carga de {@link Identificable} proporcionando un serializador por defecto
+     * para los subtipos de {@link AbstractNombrable} ({@code id} y {@code nombre}).
+     * 
+     * @param <K>
+     *            Tipo del identificador para {@code Identificable<K>}
+     * @param <T>
+     *            Tipo de la {@code claseMapa}
+     * @param <S>
+     *            Tipo de la especializacion. Puede ser el igual a {@code T}
+     * @param deserializador
+     *            {@code Deserializador} encargado de deserializar desde archivo
+     * @param rutaArchivo
+     *            ruta al archivo con los datos
+     * @param claseMapa
+     *            Clase que sirve para mapear el recurso (normalmente la mas generica)
+     * @param claseEspecializacion
+     *            Clase subtipo de claseMapa para gestionar recursos mas especializados
+     * @param gestor
+     *            Gestor que administrara los recursos a cargar
+     */
+    <K extends Comparable<K>, T extends Identificable<K> & Nombrable, S extends T> void cargarNombrables(Deserializador deserializador,
+            String rutaArchivo, Class<T> claseMapa, Class<S> claseEspecializacion, GestorIdentificables gestor);
+
+    /**
+     * Facilita la carga de {@link Identificable} proporcionando un serializador por defecto
+     * para los subtipos de {@link AbstractNombrable} ({@code id} y {@code nombre}).
+     * 
+     * @param <K>
+     *            Tipo del identificador para {@code Identificable<K>}
+     * @param <T>
+     *            Tipo de la {@code claseMapa}
+     * @param <S>
+     *            Tipo de la especializacion. Puede ser el igual a {@code T}
+     * @param rutaArchivo
+     *            ruta al archivo con los datos
+     * @param claseMapa
+     *            Clase que sirve para mapear el recurso (normalmente la mas generica)
+     * @param claseEspecializacion
+     *            Clase subtipo de claseMapa para gestionar recursos mas especializados
+     * @param gestor
+     *            Gestor que administrara los recursos a cargar
+     */
+     default <K extends Comparable<K>, T extends Identificable<K> & Nombrable, S extends T> void cargarNombrables(String rutaArchivo,
+            Class<T> claseMapa, Class<S> claseEspecializacion, GestorIdentificables gestor) {
+        cargarNombrables(getDeserializadorDefecto(), rutaArchivo, claseMapa, claseEspecializacion, gestor);
+    }
+
+    /**
+     * Sobrecarga de {@link CargadorIdentificables#cargarNombrables(String, Class, Class, GestorIdentificables)}
+     * utilizando un {@link ServicioEntidad}.
+     * 
+     * @param <K>
+     *            Tipo del identificador para {@code Identificable<K>}
+     * @param <T>
+     *            Tipo de la {@code claseMapa}
+     * @param <S>
+     *            Tipo de la especializacion. Puede ser el igual a {@code T}
+     * @param rutaArchivo
+     *            ruta al archivo con los datos
+     * @param claseMapa
+     *            Clase que sirve para mapear el recurso (normalmente la mas generica)
+     * @param claseEspecializacion
+     *            Clase subtipo de claseMapa para gestionar recursos mas especializados
+     * @param servicio
+     *            {@code ServicioEntidad} que administrara los recursos a cargar
+     */
+    default <K extends Comparable<K>, T extends Identificable<K> & Nombrable, S extends T> void cargarNombrables(String rutaArchivo,
+            Class<T> claseMapa, Class<S> claseEspecializacion, ServicioEntidad servicio) {
+        cargarNombrables(rutaArchivo, claseMapa, claseEspecializacion, servicio.getGestorNombrables());
+    }
 }
